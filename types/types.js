5c6c01575ce17eb5569f060871e5dbd038fc804c
Ismael Oliveira <ismael6991@live.com>
2023-02-03 18:51:47 -0300

    O Tipo Never

--- types/types.js
+++ types/types.js
@@ -133,3 +133,19 @@ if (typeof valor === 'number') {
 else {
     console.log(typeof valor);
 }
+// Never
+function falha(msg) {
+    throw new Error(msg);
+}
+const produto = {
+    nome: 'Sabão',
+    preco: -1,
+    validarProduto() {
+        if (!this.nome || this.nome.trim().length == 0)
+            falha('Precisa ter um nome');
+        if (this.preco <= 0) {
+            falha('Preço inválido!');
+        }
+    },
+};
+produto.validarProduto();
