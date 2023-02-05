cac8dece0d115cd03460cd10e3d8a2c48ffbf4c9
Ismael Oliveira <ismael6991@live.com>
2023-02-05 20:25:04 -0300

    Operador Rest & Spread com Tuplas

--- ecmascript/ecmascrpit.js
+++ ecmascript/ecmascrpit.js
@@ -74,3 +74,14 @@ const numeros = retornarArray(1, 2, 4, 6, 8);
 console.log(numeros);
 // Rest e Spread
 console.log(retornarArray(...numbers));
+// Rest & Spread(Tupla)
+const tupla = [1, 'abc', false];
+function tuplaParam1(a, b, c) {
+    console.log(`1) ${a} ${b} ${c}`);
+}
+tuplaParam1(...tupla);
+function tuplaParam2(...params) {
+    //   console.log(Array.isArray(params))
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
+}
+tuplaParam2(...tupla);
