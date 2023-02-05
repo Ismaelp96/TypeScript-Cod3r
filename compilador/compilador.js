a24305a8b5075ac83d7585533bc5a2da06c1106b
Ismael Oliveira <ismael6991@live.com>
2023-02-05 09:49:16 -0300

    sourceMap, noImplicitAny

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -1,6 +1,15 @@
 "use strict";
+// noEmitOnError -> não emite o arquivo JS quando ocorre um erro no TS
 let canal = 'Gaveta';
 let inscritos = 610234;
 // canal = inscritos
 console.log(`Canal = ${canal}`);
 // let nome = 'Pedro'
+//# SourceMap=compilador.js.map
+// noImplicitAny
+function soma(a, b) {
+    return a + b;
+}
+let qualquerCoisa;
+qualquerCoisa = 12;
+qualquerCoisa = 'abc';
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -1,6 +1,16 @@
+// noEmitOnError -> não emite o arquivo JS quando ocorre um erro no TS
 let canal: string = 'Gaveta'
 let inscritos: number = 610234
 
 // canal = inscritos
 console.log(`Canal = ${canal}`)
 // let nome = 'Pedro'
+//# SourceMap=compilador.js.map
+// noImplicitAny
+function soma(a: any, b: any) {
+  return a + b
+}
+
+let qualquerCoisa
+qualquerCoisa = 12
+qualquerCoisa = 'abc'
