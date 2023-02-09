0b1054120958436801789ddbb6a3debc69e611ca
Ismael Oliveira <ismael6991@live.com>
2023-02-08 21:17:19 -0300

    Instalando System JS 0.x e Carregando módulos

--- modulos/modulos.ts
+++ modulos/modulos.ts
@@ -1,5 +1,5 @@
 import { areaRetangulo } from './retangulo'
 import { areaCircunferencia } from './circunferencia'
-
+console.log('Módulo carregado!!!')
 console.log(areaRetangulo(7, 8))
 console.log(areaCircunferencia(2))
