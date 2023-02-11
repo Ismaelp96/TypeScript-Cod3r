75187f6aee1599d117a5524fb6068fbe9262710a
Ismael Oliveira <ismael6991@live.com>
2023-02-11 11:38:43 -0300

    Porque usar?

---
+++ generics/generics.js
@@ -0,0 +1,8 @@
+"use strict";
+function echo(objeto) {
+    return objeto;
+}
+console.log(echo('João').length);
+console.log(echo(2));
+console.log(echo({ nome: 'João', idade: 2 }));
+//# sourceMappingURL=generics.js.map
---
+++ generics/generics.ts
@@ -0,0 +1,7 @@
+function echo(objeto: any) {
+  return objeto
+}
+
+console.log(echo('João').length)
+console.log(echo(2))
+console.log(echo({ nome: 'João', idade: 2 }))
