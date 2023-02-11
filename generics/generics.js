a0094239917b59c22efdbae842841e8867168eef
Ismael Oliveira <ismael6991@live.com>
2023-02-11 13:39:49 -0300

    Criando uma Função com Generics

--- generics/generics.js
+++ generics/generics.js
@@ -5,4 +5,11 @@ function echo(objeto) {
 console.log(echo('João').length);
 console.log(echo(2));
 console.log(echo({ nome: 'João', idade: 2 }));
+// Usando Generics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado('Paulo').length);
+console.log(echoMelhorado(24));
+console.log(echoMelhorado({ nome: 'Paulo', idade: 24 }));
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -1,7 +1,14 @@
 function echo(objeto: any) {
   return objeto
 }
-
 console.log(echo('João').length)
 console.log(echo(2))
 console.log(echo({ nome: 'João', idade: 2 }))
+
+// Usando Generics
+function echoMelhorado<TIPO>(objeto: TIPO): TIPO {
+  return objeto
+}
+console.log(echoMelhorado('Paulo').length)
+console.log(echoMelhorado<number>(24))
+console.log(echoMelhorado({ nome: 'Paulo', idade: 24 }))
